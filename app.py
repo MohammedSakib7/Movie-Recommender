1063628823045543fda3e58b44c6561b3bdecdf1
Md Sakib <sakib@Sakibs-Macbook.local>
2023-08-24 12:55:17 -0400

    login.html form created

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, url_for, request, session, flask 
 from flask_session import sessions
 
 app = Flask(__name__)
@@ -6,5 +6,12 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    render_template(login.html)
+
+
 if __name__ == "__main__":
     app.run()
