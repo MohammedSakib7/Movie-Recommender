4cba5d36b18c63a9cb98db016666ef96edc65a31
Md Sakib <sakib@Sakibs-Macbook.local>
2023-08-24 14:16:37 -0400

    created design doc and sessions and user template

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask, redirect, render_template, url_for, request, session, flask 
-from flask_session import sessions
+import sqlite3
+from flask import Flask, redirect, render_template, url_for, request, session, flash
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+from helpers import login_required
+import datetime as d
 
 app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.route("/")
 def index():
@@ -10,8 +18,21 @@ def index():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    render_template(login.html)
+    if request.method == "POST" :
+        user = request.form["username"]
+        session["user"] = user
+        return redirect(url_for("user"))
+    else:
+        return render_template("login.html")
+
+@app.route("/user")
+def user():
+    if "user" in session:
+        user = session["user"]
+        return render_template("user.html", user=user)
+    else:
+        return render_template(url_for("login"))
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
