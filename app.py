93eccbeda09da66c86d705800d4c3a8079aa22c4
Md Sakib <sakib@Sakibs-Macbook.local>
2023-08-24 16:17:29 -0400

    adding logo

--- app.py
+++ app.py
@@ -2,20 +2,35 @@ import sqlite3
 from flask import Flask, redirect, render_template, url_for, request, session, flash
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
-from helpers import login_required
 import datetime as d
 
+# Import login decorator from helpers function
+from helpers import login_required
+
+# Initialize flask framework
 app = Flask(__name__)
 
+# Use filesystem instead of signed cookies to store session information on server side
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+# API key for OMDB movie metadata
+
+
+# API key for Youtube to display trailers for movies on a video player
+
+
+# Initialized SQL database that stores liked movies on a table along with movie information from OMDB movie database, maybe include my own tables of movie information from IMDB to query easily
+
+
+# Basic Homepage displaying movie information. If user is signed in, displays a list of recommended movies to watch along side other top movies.
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
+# User authentication using login decorator from helpers.py and save user sessions in flask_session as files
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST" :
@@ -25,7 +40,9 @@ def login():
     else:
         return render_template("login.html")
 
+# This might be removed but this displays a homepage for users who are logged in
 @app.route("/user")
+@login_required
 def user():
     if "user" in session:
         user = session["user"]
